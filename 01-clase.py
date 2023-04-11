0b3f1e59413cc09068fb77c1ca9efc9f1c9597a3
Ricardo G <ricardogon174@gmail.com>
2023-04-11 14:49:29 -0400

    Este es un commite

---
+++ 01-clase.py
@@ -0,0 +1,19 @@
+estatura = 1.71
+peso = 70
+complejo = 1 + 4j
+edad = 18
+
+print("\n","\n","\n")
+print(f"Mi estatura es de {estatura} y mi pero es de {peso}")
+print("impresion de un numero complejo: ",complejo)
+
+print("transformar un valor real a entero: ",int(peso))
+print("transformar un valor entero a real: ",int(edad))
+
+imc = peso / (estatura**2)
+
+print("Mi IMC es de: ",imc,"\n")
+
+print("Mi IMC es de: {:.2f}".format(imc),"\n")
+
+print("\n","\n","\n")
--- 01-variable.py
+++ 01-variable.py
@@ -2,3 +2,4 @@ nombre = 'Ricardo'
 print("hola soy ",nombre)
 edad = 18
 print("tengo ",edad," años de edades")
+
