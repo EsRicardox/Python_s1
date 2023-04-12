7c34a09d210c588745071ff54b2c9e460c8219ed
Ricardo G <ricardogon174@gmail.com>
2023-04-12 16:49:09 -0400

    Este es un commite

--- Reto-Clase.py
+++ Reto-Clase.py
@@ -1,4 +1,4 @@
-nombre=(input("cual es tu edad?"))
-edad= int(input("Cual es tu nombre?"))
+nombre=(input("cual es tu nombre?"))
+edad= int(input("Cual es tu edad?"))
 total_años = edad+20
 print("Hola mi nombre es ",nombre,"tengo ",edad," años y en 20 años tendre ",total_años," años")
